a613678032340e92b897a91e899d9ab19b813c22
Seoa Kim <seoa521@gmail.com>
2023-09-25 17:48:13 +0900

    [feat] 채팅방 리스트 UI 구현

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaRegSmileWink } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import { Button, Modal, Form } from "react-bootstrap";
 
-function ChatRooms() {
-  return <div>ChatRooms</div>;
+export class ChatRooms extends Component {
+  state = { shwo: false };
+
+  handleClose = () => this.setState({ show: false });
+  handleShow = () => this.setState({ show: true });
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <FaRegSmileWink style={{ marginRight: 3 }} />
+          CHAT ROOMS (1)
+          <FaPlus
+            onClick={this.handleShow}
+            style={{ position: "absolute", right: 0, cursor: "pointer" }}
+          />
+        </div>
+        {/* ADD CHAT ROOM MODAL */}
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a chat room</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>방 이름</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter a chat room name"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>방 설명</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter a chat room description"
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default ChatRooms;
