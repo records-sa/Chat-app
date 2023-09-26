e401b908112935de8e52b078507e4ce55569bdd7
Seoa Kim <seoa521@gmail.com>
2023-09-26 12:43:16 +0900

    [feat] 메시지 헤더 UI 구현

--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,7 +1,97 @@
 import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  Form,
+  Image,
+  Accordion,
+  Card,
+} from "react-bootstrap";
+import { useAccordionButton } from "react-bootstrap/AccordionButton";
+import { FaLock } from "react-icons/fa";
+import { MdFavorite } from "react-icons/md";
+import { AiOutlineSearch } from "react-icons/ai";
+
+function CustomToggle({ children, eventKey }) {
+  const decoratedOnClick = useAccordionButton(eventKey, () =>
+    console.log("totally custom!")
+  );
+
+  return (
+    <button type="button" onClick={decoratedOnClick}>
+      {children}
+    </button>
+  );
+}
 
 function MessageHeader() {
-  return <div>MessageHeader</div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "170px",
+        border: ".2rem solid #ececec",
+        borderRadius: "4px",
+        padding: "1rem",
+        marginBottom: "1rem",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col>
+            <h2>
+              <FaLock /> ChatRoomName <MdFavorite />
+            </h2>
+          </Col>
+          <Col>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">
+                <AiOutlineSearch />
+              </InputGroup.Text>
+              <Form.Control
+                placeholder="Search Messages"
+                aria-label="Search"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Image src="" /> user name
+          </div>
+        </Row>
+        <Row>
+          <Col>
+            <Accordion>
+              <Card>
+                <Card.Header style={{ padding: "0 1rem" }}>
+                  <CustomToggle eventKey="0">Click me!</CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
+          <Col>
+            <Accordion>
+              <Card>
+                <Card.Header style={{ padding: "0 1rem" }}>
+                  <CustomToggle eventKey="0">Click me!</CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default MessageHeader;
